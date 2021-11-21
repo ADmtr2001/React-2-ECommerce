3e68660afa5c029f1633d41a4be09765857f1205
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-21 15:08:14 +0200

    add AboutPage rendering

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,34 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Repellendus deserunt libero atque perferendis sed architecto quae
+            alias, velit vel, laborum molestias. Dolore nihil itaque pariatur
+            perspiciatis et exercitationem sint sunt. Cumque ad perferendis illo
+            facere? Vitae hic mollitia libero voluptates aperiam velit tempora
+            est cumque quas ipsa, perspiciatis voluptatibus reiciendis porro
+            soluta quidem dicta laudantium excepturi quasi deserunt accusantium
+            itaque.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +56,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
