0989ae007a3ad7e88a9b2f344a0ca3dd04afa6e7
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-23 02:08:12 +0200

    add Auth0

--- src/index.js
+++ src/index.js
@@ -8,13 +8,25 @@ import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+// dev-2847dwro.us.auth0.com
+// DrVhrfNKhf1fwgVXBA2vGebw06UnrPim
+
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain='dev-2847dwro.us.auth0.com'
+    clientId='DrVhrfNKhf1fwgVXBA2vGebw06UnrPim'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
