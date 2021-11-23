31fa9049060a8186f5ff67d4598c0cac619e5824
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-23 10:07:27 +0200

    add AuthWrapper to fix Auth0 bug

--- src/App.js
+++ src/App.js
@@ -12,35 +12,38 @@ import {
   Products,
   PrivateRoute,
 } from "./pages";
+import AuthWrapper from "./pages/AuthWrapper";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route exact path='/about'>
-          <About />
-        </Route>
-        <Route exact path='/cart'>
-          <Cart />
-        </Route>
-        <Route exact path='/products'>
-          <Products />
-        </Route>
-        <Route exact path='/products/:id' children={<SingleProduct />} />
-        <PrivateRoute exact path='/checkout'>
-          <Checkout />
-        </PrivateRoute>
-        <Route path='*'>
-          <Error />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/about'>
+            <About />
+          </Route>
+          <Route exact path='/cart'>
+            <Cart />
+          </Route>
+          <Route exact path='/products'>
+            <Products />
+          </Route>
+          <Route exact path='/products/:id' children={<SingleProduct />} />
+          <PrivateRoute exact path='/checkout'>
+            <Checkout />
+          </PrivateRoute>
+          <Route path='*'>
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
